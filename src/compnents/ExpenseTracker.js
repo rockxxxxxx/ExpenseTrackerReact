24c098be349cd8fd082c284b38d6893e1da98989
Rakesh Raushan <raushanrakesh069@gmail.com>
2022-09-06 22:40:58 +0530

    Created Static Expense Tracker

---
+++ src/compnents/ExpenseTracker.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ExpenseTracker() {
+  return (
+    <>
+      <h2>Lets get Started!</h2>
+      <h3>Expense Tracker</h3>
+      <h4>Expense Items</h4>
+      <p>Food: - Rs 10</p>
+      <p>Petrol: - Rs 100</p>
+      <p>Movies: - Rs 200</p>
+    </>
+  );
+}
